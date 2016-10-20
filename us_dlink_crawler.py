a91639dfdcb0efd2137f1b0547ca40d6110909c1
MikimotoH <MikimotoH@gmail.com>
2016-10-20 02:56:59 +0000

    download file in crawlwer directly using ThreadPoolExecutor

--- us_dlink_crawler.py
+++ us_dlink_crawler.py
@@ -1,14 +1,57 @@
 # -*- coding:utf8 -*-
-from urllib import parse
-from pyquery import PyQuery as pq
-import requests
-import time
-# import json
-from datetime import datetime
 import re
-import ipdb
-# import traceback
+import os
+from urllib import parse
+from concurrent.futures import ThreadPoolExecutor
 import csv
+import time
+from datetime import datetime
+import requests
+import ipdb
+from pyquery import PyQuery as pq
+
+executor=None
+localstor='output/D-Link/ftp2.dlink.com/'
+
+
+def download(model, rev, fw_ver, fw_url):
+    from web_utils import getFileSha1, getFileMd5
+    import ftputil
+    try:
+        fname = fw_url.split('/')[-1]
+
+        def epilog(fsize, fdate):
+            if not os.path.isfile(localstor+fname):
+                sha1=None
+                md5=None
+            else:
+                sha1 = getFileSha1(localstor+fname)
+                md5 = getFileMd5(localstor+fname)
+            with open('us_dlink_filelist.csv', 'a') as fout:
+                cw = csv.writer(fout)
+                cw.writerow([model, rev, fw_ver, fw_url, fsize, fdate, sha1, md5])
+            return
+        from urllib import parse
+        fw_path = parse.urlsplit(fw_url).path
+        netloc = parse.urlsplit(fw_url).netloc
+        with ftputil.FTPHost(netloc, 'anonymous', '') as host:
+            if not host.path.isfile(fw_path):
+                print('"%s" does not exist.'%fw_path)
+                epilog(-1, None)
+                return
+            fsize = host.path.getsize(fw_path)
+            fdate = host.path.getmtime(fw_path)
+            if os.path.isfile(localstor+fname) and os.path.getsize(localstor+fname)==fsize:
+                print('%(fname)s already exists'%locals())
+                epilog(fsize,fdate)
+                return
+            print('Start downloading %(fw_url)s'%locals())
+            host.download(fw_path, localstor+fname)
+            print('Finised downloading %(fw_url)s'%locals())
+            epilog(fsize,fdate)
+            return
+    except Exception as ex:
+        print(ex)
 
 
 def extract_date(txt):
@@ -30,18 +73,6 @@ def extract_fw_ver(txt):
         return None
 
 
-def csvinit():
-    with open('us_dlink_filelist.csv', 'w') as fout:
-        cw = csv.writer(fout)
-        cw.writerow(['model', 'rev', 'fw_ver', 'fw_url', 'date'])
-
-
-def csvwrite(row):
-    with open('us_dlink_filelist.csv', 'a') as fout:
-        cw = csv.writer(fout)
-        cw.writerow(row)
-
-
 def crawl_prod(prod_url, model):
     d = pq(url=prod_url)
     revs = [(_.attrib['value'], _.text_content().strip()) for _ in d('select#ddlHardWare option')]
@@ -58,56 +89,34 @@ def crawl_prod(prod_url, model):
                     fw_ver = extract_fw_ver(fil['name'])
                     date = extract_date(fil['date'])
                     print('%s %s %s %s %s'%(model, revname, fw_ver, fil['url'], date))
-                    csvwrite([model,revname, fw_ver, fil['url'], date])
-
-
-"http://support.dlink.com/ProductInfo.aspx?m=DWL-5000AP"
-# no need ajax
-# only one option in select.ddlHardWare
-
-"http://support.dlink.com/ProductInfo.aspx?m=DFE-530TX%2B"
-# need ajax get different version
-# requests.Request('http://support.dlink.com/ajax/ajax.ashx?d=1473748848724&action=productfile&'\
-#                 'lang=en-US&ver=1073&ac_id=1', headers=
-#                 {'Referer':'http://support.dlink.com/ProductInfo.aspx?m=DFE-530TX%2B'})
-# d = int(time.time()*1000)
-# ver=1073
-#  ```html
-#     <select name="ddlHardWare" id="ddlHardWare" class="downloadddl">
-#      <option value="">--Please select--</option>
-#       <option value="1073">F</option>
-#       <option value="1072">G</option>
-# ```
-# response is JSON format, like
-#  In [48]: js['item'][0]['file'][1]['url']
-#  Out[48]: 'ftp://FTP2.DLINK.COM/PRODUCTS/DFE-530TXPLUS/REVA/DFE-530TXPLUS_QIG_1.00_WIN95_EN.PDF'
-#
-#  In [49]: js['item'][0]['file'][1]['filetypename']
-#  Out[49]: 'Quick Install Guide'
-#
-#  In [50]: js['item'][1]['file'][1]['filetypename']
-#  Out[50]: 'User Manual'
-#
-#  In [52]: js['item'][2]['file'][0]['filetypename']
-#  Out[52]: 'Datasheet'
-#
-#  In [53]: js['item'][3]['file'][0]['filetypename']
-#  Out[53]: 'Driver'
-#
-"http://support.dlink.com/ajax/ajax.ashx?d=1473751407969&action=productfile&lang=en-US&ver=919&ac_id=1"
-"http://support.dlink.com/ajax/ajax.ashx?d=1473751433239&action=productfile&lang=en-US&ver=920&ac_id=1"
+                    try:
+                        global executor
+                        executor.submit(download, model, revname, fw_ver, fil['url'])
+                    except Exception as ex:
+                        print(ex)
+                        import traceback
+                        traceback.print_exc()
 
 
 def main():
+    global executor
+    executor=ThreadPoolExecutor()
+
+    os.makedirs(localstor, exist_ok=True)
+
+    with open('us_dlink_filelist.csv', 'w') as fout:
+        cw = csv.writer(fout)
+        cw.writerow(['model', 'rev', 'fw_ver', 'fw_url', 'fsize', 'fdate', 'sha1', 'md5'])
+
     start_url="http://support.dlink.com/AllPro.aspx?type=all"
     d = pq(url=start_url)
     # all 442 models
     models = [_.text_content().strip() for _ in d('tr > td:nth-child(1) > .aRedirect')]
-    csvinit()
 
     for model in models:
         prod_url = "http://support.dlink.com/ProductInfo.aspx?m=%s"%parse.quote(model)
         crawl_prod(prod_url, model)
+    executor.shutdown(True)
 
 
 if __name__=='__main__':
