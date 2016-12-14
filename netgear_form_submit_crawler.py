0831f8ece1b7b1de11c821a25ac0bcde84fe1fd9
MikimotoH <MikimotoH@gmail.com>
2016-12-14 14:01:56 +0000

    download_file and csv

--- netgear_form_submit_crawler.py
+++ netgear_form_submit_crawler.py
@@ -1,17 +1,32 @@
-import requests
-from form_submit import form_submit
+import os
+import csv
+import re
 import traceback
+from concurrent.futures import ThreadPoolExecutor
+from dateutil.parser import parse as parse_date
+import requests
 from lxml import html
+from form_submit import form_submit
+from web_utils import getFileSha1, getFileMd5
+from urllib.parse import urlsplit
 
 
+executor = None
+dlDir = './output/netgear/downloadcenter.netgear.com_form_submit/'
 startCat=0
 startFam=0
 startProd=0
 startFirmware=0
 def main():
+    global executor
     try:
         session = requests.Session()
+        executor = ThreadPoolExecutor()
+        os.makedirs(dlDir, exist_ok=True)
         url='http://downloadcenter.netgear.com'
+        with open('netgear_filelist.csv', 'w') as fout:
+            cw = csv.writer(fout)
+            cw.writerow(['model', 'fw_ver', 'fileName', 'fw_url', 'fw_date', 'fileSize', 'sha1', 'md5'])
         response = session.get(url=url)
         root = html.fromstring(response.text)
         href = root.xpath(".//a[@id='ctl00_ctl00_ctl00_mainContent_localizedContent_bodyCenter_BasicSearchPanel_btnAdvancedSearch']/@href")
@@ -24,6 +39,8 @@ def main():
         walkCategories(session, resp2)
     except BaseException as ex:
         traceback.print_exc()
+    finally:
+        executor.shutdown(True)
 
 
 def strip_js(url):
@@ -126,6 +143,47 @@ def walkFirmwares(response, product):
                 url = href[0]
                 model = product.xpath(".//text()")[0]
                 print('model="%s", desc="%s", url=%s'%(model, desc, url))
+                global executor
+                executor.submit(download_file, model, desc, url)
+    except BaseException as ex:
+        traceback.print_exc()
+
+
+def download_file(model, desc, fw_url):
+    try:
+        resp = requests.get(url=fw_url, stream=True)
+        if 'Content-Length' in resp.headers:
+            fileSize = int(resp.headers['Content-Length'])
+            print('fileSize=', fileSize)
+        else:
+            fileSize=None
+        try:
+            fw_ver = re.search(r'\d+(\.\d+)+', desc).group(0)
+        except AttributeError:
+            fw_ver = ''
+        fileName = os.path.basename(urlsplit(fw_url).path)
+        print('fileName=', fileName)
+        if 'Last-Modified' in resp.headers:
+            fw_date= resp.headers['Last-Modified']
+            fw_date = parse_date(fw_date)
+        else:
+            fw_date = None
+        if os.path.isfile(dlDir+fileName) \
+                and fileSize==os.path.getsize(dlDir+fileName):
+            print('already downloaded: ', fileName)
+        else:
+            print('start downloading: ', fw_url)
+            with open(dlDir+fileName+'.downloading', 'wb') as fout:
+                for chunk in resp.iter_content(8192):
+                    fout.write(chunk)
+            os.rename(dlDir+fileName+'.downloading', dlDir+fileName)
+            print('finished downloading: ', fw_url)
+        sha1 = getFileSha1(dlDir+fileName)
+        md5 = getFileMd5(dlDir+fileName)
+        fileSize = os.path.getsize(dlDir+fileName)
+        with open('netgear_filelist.csv', 'a') as fout:
+            cw = csv.writer(fout)
+            cw.writerow([model, fw_ver, fileName, fw_url, fw_date, fileSize, sha1, md5])
     except BaseException as ex:
         traceback.print_exc()
 
